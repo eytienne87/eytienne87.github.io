485bcb21b77c0d3fbfab238885148715f35c7600
Etienne Tessier <etienne_tessier100@hotmail.ca>
2022-02-01 15:06:00 -0500

    implemented event listener so that when the video progress bar is clicked, the length is modified and the video current time is updated

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,6 +2,40 @@ const video = document.querySelector('.player__video');
 const playButton = document.querySelector('.toggle');
 const volumeInput = document.querySelector('input[name=volume]');
 const playbackRateInput = document.querySelector('input[name=playbackRate]');
+const minusSecondsButton = document.querySelector("button[data-skip='-10']");
+const addSecondsButton = document.querySelector("button[data-skip='25']");
+const progressBarFilled = document.querySelector('.progress__filled');
+const progressBar = document.querySelector('.progress')
+
+const progressHandler = (event) => {
+  const progressBarLength = progressBar.offsetWidth;
+  const areaClicked = event.offsetX;
+  const videoDuration = video.duration;
+
+  // Update the yellow progress bar based on where the user clicked
+  const percentageOfBarClicked = (Math.round(((areaClicked / progressBarLength) * 100) * 10) / 10);
+  progressBarFilled.style.flexBasis = `${percentageOfBarClicked}%`;
+
+  // Update the current time of the video based on the click
+  const timestamp = (percentageOfBarClicked / 100) * videoDuration;
+  video.currentTime = timestamp;
+};
+
+
+const progressBarHandler = () => {
+  const duration = video.duration;
+  const currentTime = video.currentTime;
+  const percentageTimeElapsed = (Math.round(((currentTime / duration) * 100) * 10) / 10);
+  progressBarFilled.style.flexBasis = `${percentageTimeElapsed}%`
+};
+
+const addButtonHandler = () => {
+  video.currentTime += 25
+}
+
+const minusButtonHandler = () => {
+  video.currentTime -= 10;
+};
 
 const volumeHandler = () => {
   const volumeInputValue = volumeInput.value;
@@ -20,14 +54,8 @@ const playButtonHandler = () => {
 }
 
 const togglePlayButton = () => {
-  const pausedState = playButton.innerHTML === '►';
-
-  if (pausedState) {
-    playButton.innerHTML = 'l l';
-    playButton.style.fontWeight = 'bolder';
-  } else {
-    playButton.innerHTML = '►';
-  }
+  const playButtonIcon = event.currentTarget.paused ? '►' : '❚ ❚';
+  playButton.innerHTML = playButtonIcon;
 };
 
 const playPauseToggle = () => {
@@ -38,15 +66,21 @@ const playPauseToggle = () => {
   }
 };
 
-const videoClickHandler = () => {
-  playPauseToggle();
-};
-
-video.addEventListener('click', videoClickHandler);
+video.addEventListener('click', playPauseToggle);
 playButton.addEventListener('click', playButtonHandler);
+
+video.addEventListener('play', togglePlayButton);
+video.addEventListener('pause', togglePlayButton);
 
 volumeInput.addEventListener('input', volumeHandler);
 volumeInput.addEventListener('change', volumeHandler);
 
 playbackRateInput.addEventListener('input', playbackRateHandler);
 playbackRateInput.addEventListener('change', playbackRateHandler);
+
+minusSecondsButton.addEventListener('click', minusButtonHandler);
+addSecondsButton.addEventListener('click', addButtonHandler);
+
+video.addEventListener('timeupdate', progressBarHandler);
+
+progressBar.addEventListener('click', progressHandler)
